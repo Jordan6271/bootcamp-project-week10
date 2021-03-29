248af3bb6e9664ac5ab75ee3edd59696ea988b5d
Jordan6271 <jordan.robertson-browne@mattressonline.co.uk>
2021-03-29 22:59:35 +0100

    Create navbar look for landing page

--- noteable/src/Noteable/Noteable.js
+++ noteable/src/Noteable/Noteable.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import Board from './Board/Board';
 import NewNote from './Note/NewNote';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function Noteable() {
     return (
         <div className="noteable">
+            <Navbar bg="light" expand="md">
+                <Navbar.Brand>Noteable</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        Board
+                        New Note
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
             <NewNote />
             <Board />
         </div>
